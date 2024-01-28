bc949ef0af5fba77785ba838fd4ae6ed05580119
Kevin O'Sullivan <koschaos@gmail.com>
2024-01-28 01:06:04 -0500

    Clean, tighten up code

--- t2/main.js
+++ t2/main.js
@@ -1,43 +1,31 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js';
-import {
-  getDatabase,
-  ref,
-  update,
-} from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js';
+import { getDatabase, ref, update, } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js';
 import { firebaseConfig } from './firebase-config.js';
 
-// Initialize Firebase with your config
-initializeApp(firebaseConfig);
-
-// const redirectTimer = 2000; // 2 seconds
-
-// Get parameters from the URL
 const urlParams = new URLSearchParams(window.location.search);
 const userId = urlParams.get('id');
-const redirectUrl = urlParams.get('redirect');
-const urlKey = redirectUrl ? redirectUrl.replace(/[^a-zA-Z0-9\-]/g, "-") : null;
+let redirectUrl = urlParams.get('redirect');
+if (!redirectUrl) {
+  redirectUrl = "https://koschaos.github.io/blog";
+}
+const urlKey =  redirectUrl.replace(/[^a-zA-Z0-9\-]/g, "-");
 // console.log(`DEBUG: ${userId ? userId : "null"}, ${redirectUrl ? redirectUrl : "null"}, ${urlKey ? urlKey : "null"}`);
 
 if (userId && urlKey) {
-  // Add/update the "id" to the Firebase realtime database
+  initializeApp(firebaseConfig);
   const database = getDatabase();
-  update(ref(database, 'tracker/' + userId), {
-    [urlKey]: 1
+  update(ref(database, 'tracker/' + urlKey), {
+    [userId]: 1
   })
   .then(() => {
-    // Data saved successfully!
-    // console.log(`DEBUG: ${urlKey} for ${userId} successfully saved to database`)
+    // console.log(`DEBUG: ${userId} for ${urlKey} successfully saved to database`);
   })
   .catch((error) => {
     console.error(error);
   })
   .finally(() => {
-    window.location.href = redirectUrl ? redirectUrl : "https://koschaos.github.io/blog";
+    window.location.href = redirectUrl;
   });
 } else {
-  window.location.href = redirectUrl ? redirectUrl : "https://koschaos.github.io/blog";
+  window.location.href = redirectUrl;
 }
-
-// setTimeout(() => {
-//   window.location.href = redirectUrl ? redirectUrl : "https://koschaos.github.io/blog";
-// }, redirectTimer);
